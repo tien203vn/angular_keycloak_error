31c20f3a53d4e83407ab61cf088d98ae0c5e1d91
tien203vn <leminhtien.it.vn@gmail.com>
2025-01-03 15:29:32 +0700

    change authenticated component

---
+++ src/app/web/page/module/common/authenticated/authenticated.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-authenticated',
+  standalone: true,
+  imports: [],
+  templateUrl: './authenticated.component.html',
+  styleUrl: './authenticated.component.css'
+})
+export class AuthenticatedComponent {
+
+}
